a33a56fc1cb66be810480bc23719f31e8fcaacd7
joy <ss13867776368@163.com>
2020-01-07 15:02:15 +0800

    update svd.py

--- lib/classic_algo/svd.py
+++ lib/classic_algo/svd.py
@@ -1,4 +1,5 @@
 import math
+import time
 import numpy as np
 from cfg.config import config as cfg
 
@@ -19,11 +20,12 @@ class SVD(object):
         self.n_users = cfg.N_users
         self.n_items = cfg.N_items
 
-    def train_sgd(self,):
+    def train_sgd(self):
         # p, q correspond to user_matrix and item_matrix
         p_mat = np.random.random_sample([self.n_users, self.feature_num])
         q_mat = np.random.random_sample([self.n_items, self.feature_num])
-        
+
+        start = time.time()
         for epoch in range(self.epoch_num):
             for u in range(self.n_users):
                 for i in range(self.n_items):
@@ -33,3 +35,28 @@ class SVD(object):
                         q_mat[i, :] += self.gamma * (e_ui * p_mat[u, :] - self.lamb * q_mat[i, :])
 
         return p_mat, q_mat
+
+    def train_sgd_with_bias(self):
+        # mean of all the non-zero elements
+        mu = np.mean(self.data_mat[np.nonzero(self.data_mat)])
+
+        # p, q correspond to user_matrix and item_matrix
+        p_mat = np.random.random_sample([self.n_users, self.feature_num])
+        q_mat = np.random.random_sample([self.n_items, self.feature_num])
+
+        b_u = np.ones([self.n_users])
+        b_i = np.ones([self.n_items])
+        
+        start = time.time()
+        for epoch in range(self.epoch_num):
+            for u in range(self.n_users):
+                for i in range(self.n_items):
+                    if self.data_mat[u, i] != 0:
+                        b_ui = b_u[u] + b_i[i] + mu
+                        e_ui = self.data_mat[u, i] - np.dot(q_mat.T, p_mat) - b_ui
+                        p_mat[u, :] += self.gamma * (e_ui * q_mat[i, :] - self.lamb * p_mat[u, :])
+                        q_mat[i, :] += self.gamma * (e_ui * p_mat[u, :] - self.lamb * q_mat[i, :])
+                        b_u[u] += self.gamma * (e_ui - self.lamb * b_u[u])
+                        b_i[i] += self.gamma * (e_ui - self.lamb * b_i[i])
+
+        return p_mat, q_mat, b_u, b_i
