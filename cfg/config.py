d8765317c1cc4e3bb2f7173ddb5402510deb23e0
joy <ss13867776368@163.com>
2020-01-09 23:03:31 +0800

    update config.py

--- cfg/config.py
+++ cfg/config.py
@@ -14,7 +14,19 @@ config.N_items = 1682
 config.N_users = 943
 config.rating_min = 1
 config.rating_max = 5
+config.epoch_num = 10
 
 # output
 config.outputs = edict()
 config.outputs.root_path = './outputs'
+
+# feature number
+# config.feature_num_list = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+config.exp = edict()
+config.exp.feature_num_list = [2]
+config.exp.dataset_list = ['u1', 'u2']
+config.exp.algo_list = ['svd_bias']
+
+config.svd = edict()
+config.svd.gamma = 0.05
+config.svd.lamb = 0.1
