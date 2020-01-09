808d6aaf2f67feb847b66469b15d2c5d46e5dabf
joy <ss13867776368@163.com>
2020-01-09 23:02:44 +0800

    update utils, including fix bugs in create_logger.py and add saveData() in data_util.py

--- lib/utils/create_logger.py
+++ lib/utils/create_logger.py
@@ -3,28 +3,23 @@ import logging
 import time
 
 
-def create_logger(root_output_path, cfg, mdp_name, temp_flie=False):
+def create_logger(root_output_path, algorithm, dataset, temp_flie=False):
     # set up logger
     if not os.path.exists(root_output_path):
         os.makedirs(root_output_path)
     assert os.path.exists(root_output_path), '{} does not exist'.format(root_output_path)
 
-    cfg_name = os.path.basename(cfg).split('.')[0]
-    config_output_path = os.path.join(root_output_path, '{}'.format(cfg_name))
-    if not os.path.exists(config_output_path):
-        os.makedirs(config_output_path)
-
-    final_output_path = os.path.join(config_output_path, '{}'.format('_'.join(mdp_name)))
+    final_output_path = os.path.join(root_output_path, str(algorithm))
     if not os.path.exists(final_output_path):
         os.makedirs(final_output_path)
-    final_output_path = os.path.join(final_output_path, time.strftime('%Y-%m-%d-%H-%M'))
+    final_output_path = os.path.join(final_output_path, str(dataset) +'_' + time.strftime('%Y-%m-%d-%H-%M'))
     if not os.path.exists(final_output_path):
         os.makedirs(final_output_path)
 
     if temp_flie:
-        log_file = 'temp_{}_{}.log'.format(cfg_name, time.strftime('%Y-%m-%d-%H-%M'))
+        log_file = 'temp_{}_{}.log'.format(algorithm, time.strftime('%Y-%m-%d-%H-%M'))
     else:
-        log_file = '{}_{}.log'.format(cfg_name, time.strftime('%Y-%m-%d-%H-%M'))
+        log_file = '{}_{}.log'.format(algorithm, time.strftime('%Y-%m-%d-%H-%M'))
     head = '%(asctime)-15s %(message)s'
     logging.basicConfig(filename=os.path.join(final_output_path, log_file), format=head)
     logger = logging.getLogger()
--- lib/utils/data_util.py
+++ lib/utils/data_util.py
@@ -4,7 +4,7 @@ import numpy as np
 from cfg.config import config as cfg
 
 
-def load_data(path):
+def loadData(path):
     data_list = []
     with open(path, 'r') as f:
         data_list = f.readlines()
@@ -13,15 +13,22 @@ def load_data(path):
     rating_mat = np.zeros([cfg.N_users, cfg.N_items], dtype=np.float32)
     for l in data_list:
         data = l.split('\t')
-        rating_mat[int(data[0]), int(data[1])] = float(data[2])
+        rating_mat[int(data[0]) - 1, int(data[1]) - 1] = float(data[2])
     return n_samples, rating_mat
 
 
-def find_path(filename, dataset='ml-100k'):
+def findPath(filename, dataset='ml-100k'):
     root_path = os.path.abspath('.')
     root_data_path = os.path.join(root_path, 'data', dataset, filename)
     return root_data_path
 
+
+def saveData(filename, data_list):
+    with open(filename, 'w') as f:
+        for data in data_list:
+            f.write(str(data))
+            f.write('\n')
+        f.close()
 
 if __name__ == '__main__':
     print(cfg)
