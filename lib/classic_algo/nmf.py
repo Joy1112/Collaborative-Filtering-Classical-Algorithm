408fe561a7824767e2e35e5fd87adf24768f511f
joy <ss13867776368@163.com>
2020-01-10 18:03:34 +0800

    update nmf.py, fix some bugs

--- lib/classic_algo/nmf.py
+++ lib/classic_algo/nmf.py
@@ -1,5 +1,6 @@
 import math
 import time
+import copy
 import pickle
 import numpy as np
 from cfg.config import config as cfg
@@ -13,9 +14,10 @@ class WNMF(object):
         Zhang, S., Wang, W., Ford, J., & Makedon, F. (2006, April). Learning from incomplete ratings using non-negative matrix factorization.
         In Proceedings of the 2006 SIAM international conference on data mining (pp. 549-553). Society for Industrial and Applied Mathematics.
     """
-    def __init__(self, feature_num, epoch_num, loss_threshold=None, save_model=False):
+    def __init__(self, feature_num, epoch_num, logger, loss_threshold=None, save_model=False):
         self.feature_num = feature_num
         self.epoch_num = epoch_num
+        self.logger = logger
         self.loss_threshold = loss_threshold
         self.save_model = save_model
 
@@ -36,6 +38,10 @@ class WNMF(object):
                 shape: [n_users, n_items]
                 type: np.ndarray(np.int32)
         """
+        train_rmse_loss_list = []
+        valid_rmse_loss_list = []
+        accuracy_list = []
+
         # train_samples is the array of the non-zero samples.
         train_samples = train_data[np.nonzero(train_data)]
         # W_mat is the weight matrix, W[i, j] = 1 if train_data[i, j] > 0 else 0.
@@ -43,6 +49,8 @@ class WNMF(object):
 
         U_mat = np.random.random_sample([self.n_users, self.feature_num])
         V_mat = np.random.random_sample([self.feature_num, self.n_items])
+        U_mat_new = copy.deepcopy(U_mat)
+        V_mat_new = copy.deepcopy(V_mat)
 
         start = time.time()
         for epoch in range(self.epoch_num):
@@ -58,22 +66,41 @@ class WNMF(object):
                     break
 
             # update the model by the 'multiplicative update rules' in WNMF
-            U_mat_new = np.divide(np.dot(np.multiply(W_mat, train_data), V_mat.T), np.dot(np.multiply(W_mat, A_mat), V_mat.T))
+            U_up_factor = np.dot(np.multiply(W_mat, train_data), V_mat.T)
+            U_down_factor = np.dot(np.multiply(W_mat, A_mat), V_mat.T)
+            # U_mat_new = np.divide(np.dot(np.multiply(W_mat, train_data), V_mat.T), np.dot(np.multiply(W_mat, A_mat), V_mat.T))
+            for i in range(len(np.nonzero(U_up_factor)[0])):
+                row = np.nonzero(U_up_factor)[0]
+                col = np.nonzero(U_up_factor)[1]
+                U_mat_new[row[i], col[i]] = U_up_factor[row[i], col[i]] / U_down_factor[row[i], col[i]]
             U_mat_new = np.multiply(U_mat, U_mat_new)
-            V_mat_new = np.divide(np.dot(U_mat.T, np.multiply(W_mat, train_data)), np.dot(U_mat.T, np.multiply(W_mat, A_mat)))
+
+            # V_mat_new = np.divide(np.dot(U_mat.T, np.multiply(W_mat, train_data)), np.dot(U_mat.T, np.multiply(W_mat, A_mat)))
+            V_up_factor = np.dot(U_mat.T, np.multiply(W_mat, train_data))
+            V_down_factor = np.dot(U_mat.T, np.multiply(W_mat, A_mat))
+            for i in range(len(np.nonzero(V_up_factor)[0])):
+                row = np.nonzero(V_up_factor)[0]
+                col = np.nonzero(V_up_factor)[1]
+                V_mat_new[row[i], col[i]] = V_up_factor[row[i], col[i]] / V_down_factor[row[i], col[i]]
             V_mat_new = np.multiply(V_mat, V_mat_new)
-            U_mat = U_mat_new
-            V_mat = V_mat_new
+            U_mat = copy.deepcopy(U_mat_new)
+            V_mat = copy.deepcopy(V_mat_new)
 
             end = time.time()
 
             if eval_data.any():
                 valid_rmse_loss, accuracy = self.evaluate(train_data, eval_data, U_mat, V_mat)
-                print_and_log("Epoch {d}: totally training time {:.4f}, training RMSE: {:.4f}, validation RMSE: {:.4f}, accuracy: {:.4f}%"
-                              .format(epoch, end - start, train_rmse_loss, valid_rmse_loss, accuracy * 100))
+                train_rmse_loss_list.append(train_rmse_loss)
+                valid_rmse_loss_list.append(valid_rmse_loss)
+                accuracy_list.append(accuracy)
+
+                print_and_log("Epoch {:d}: totally training time {:.4f}, training RMSE: {:.4f}, validation RMSE: {:.4f}, accuracy: {:.4f}%"
+                              .format(epoch, end - start, train_rmse_loss, valid_rmse_loss, accuracy * 100), self.logger)
             else:
-                print_and_log("Epoch {d}: totally training time {:.4f}, training RMSE: {:.4f}"
-                              .format(epoch, end - start, train_rmse_loss))
+                print_and_log("Epoch {:d}: totally training time {:.4f}, training RMSE: {:.4f}"
+                              .format(epoch, end - start, train_rmse_loss), self.logger)
+
+        return train_rmse_loss_list, valid_rmse_loss_list, accuracy_list
 
     def evaluate(self, train_data, eval_data, U_mat, V_mat):
         """
